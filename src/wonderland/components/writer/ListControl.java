4aba56d9bdc303e5c1d807460321a11c749c6e60
Glains <lukas-kannenberg@web.de>
2015-10-15 20:15:04 +0200

    Unkown

---
+++ src/wonderland/components/writer/ListControl.java
@@ -0,0 +1,26 @@
+package wonderland.components.writer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import wonderland.general.core.BookItem;
+import wonderland.general.core.Grade;
+
+public class ListControl {
+
+	private Grade grade;
+	private List<BookItem> given = new ArrayList<>();
+	private List<BookItem> selected = new ArrayList<>();
+	
+	/**
+	 * Main Constructor.
+	 */
+	public ListControl() {
+
+	}
+	
+	public void updateGiven() {
+		
+	}
+}
+
---
+++ src/wonderland/components/writer/WriterController.java
@@ -0,0 +1,5 @@
+package wonderland.components.writer;
+
+public class WriterController {
+	
+}

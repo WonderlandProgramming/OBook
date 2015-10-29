572eeea878df1325289fa57c8a24579d8acd2615
Glains <lukas-kannenberg@web.de>
2015-10-29 17:39:42 +0100

    Added ServerControl

---
+++ src/main/java/wonderland/components/ServerControl.java
@@ -0,0 +1,11 @@
+package main.java.wonderland.components;
+
+import main.java.wonderland.components.reader.ReaderController;
+import main.java.wonderland.components.writer.WriterController;
+
+public class ServerControl {
+
+	public static final WriterController writer = new WriterController();
+	public static final ReaderController reader = new ReaderController();
+	
+}
